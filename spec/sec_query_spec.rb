bd1734b7757ad61c9aa74c60970d49c566da839f
Ty Rauber <tyrauber@mac.com>
2013-03-10 11:53:12 -0700

    Version Bump. Better test coverage.

--- spec/sec_query_spec.rb
+++ spec/sec_query_spec.rb
@@ -1,65 +1,109 @@
-
-#cfr_directors = ["Carla A. Hills", "Robert E. Rubin", "Richard E. Salomon",  "Richard N. Haass", "John P. Abizaid", "Peter Ackerman", "Fouad Ajami", "Madeleine K. Albright", "Alan S. Blinder", "Mary Boies", "David G. Bradley",  "Tom Brokaw", "Sylvia Mathews Burwell", "Kenneth M. Duberstein", "Martin S. Feldstein", "Stephen Friedman","Ann M. Fudge", "Pamela Gann", "Thomas H. Glocer",  "J. Tomilson Hill","Donna J. Hrinak", "Alberto Ibargüen", "Shirley Ann Jackson", "Henry R. Kravis", "Jami Miscik", "Joseph S. Nye",  "James W. Owens",  "Eduardo J. Padrón", "Colin L. Powell", "Penny Pritzker", "David M. Rubenstein",   "George Rupp", "Frederick W. Smith",  "Christine Todd Whitman", "Fareed Zakaria",  "Leslie H. Gelb", "Maurice R. Greenberg",  "Peter G. Peterson","David Rockefeller"]
-
-    
-
-cfr_directors = [
-    {:middle=>"A", :first=>"Carla", :last=>"Hills", :cik_id=> "0001194913"},
-    {:middle=>"E", :first=>"Robert", :last=>"Rubin", :cik_id=> "0001225178"},
-    {:middle=>"E", :first=>"Richard", :last=>"Salomon", :cik_id => "0001217109"},
-    {:middle=>"N", :first=>"Richard", :last=>"Haass", :cik_id => "0001123553"},
-    {:middle=>"P", :first=>"John", :last=>"Abizaid", :cik_id => "0001425152"},
-    {:first=>"Peter", :last=>"Ackerman", :cik_id => "0001111564"},
-    {:first=>"Fouad", :last=>"Ajami"},
-    {:middle=>"K", :first=>"Madeleine", :last=>"Albright"},
-    {:middle=>"S", :first=>"Alan", :last=>"Blinder"},
-    {:first=>"Mary", :last=>"Boies", :cik_id => "0001291933"},
-    {:middle=>"G", :first=>"David", :last=>"Bradley", :cik_id=> "0001106734"},
-    {:first=>"Tom", :last=>"Brokaw"},
-    {:first=>"Sylvia", :last=>"Burwell"},
-    {:middle=>"M", :first=>"Kenneth", :last=>"Duberstein", :cik_id=> "0001179625"},
-    {:middle=>"S", :first=>"Martin", :last=>"Feldstein", :cik_id=> "0001236596"},
-    {:first=>"Stephen", :last=>"Friedman", :cik_id=> "0001029607"},
-    {:middle=>"M", :first=>"Ann", :last=>"Fudge", :cik_id=> "0001198098"},
-    {:first=>"Pamela", :last=>"Gann"},
-    {:middle=>"H", :first=>"Thomas", :last=>"Glocer", :cik_id=> "0001140799"},
-    {:middle=>"J", :first=>"J.", :last=>"Hill"},
-    {:middle=>"J", :first=>"Donna", :last=>"Hrinak", :cik_id=> "0001296811"},
-    {:first=>"Alberto", :last=>"Ibarguen", :cik_id=> "0001339732"},
-    {:first=>"Shirley", :last=>"Jackson", :cik_id=> "0001168019"},
-    {:middle=>"R", :first=>"Henry", :last=>"Kravis", :cik_id=> "0001081714"},
-    {:first=>"Jami", :last=>"Miscik"},
-    {:middle=>"S", :first=>"Joseph", :last=>"Nye", :cik_id=> "0001299821"},
-    {:middle=>"W", :first=>"James", :last=>"Owens", :cik_id=> "0001443909"},
-    {:middle=>"J", :first=>"Eduardo", :last=>"Padron"},
-    {:middle=>"L", :first=>"Colin", :last=>"Powell"},
-    {:first=>"Penny", :last=>"Pritzker", :cik_id=> "0001087398"},
-    {:middle=>"M", :first=>"David", :last=>"Rubenstein"},
-    {:first=>"George", :last=>"Rupp"},
-    {:middle=>"W", :first=>"Frederick", :last=>"Smith", :cik_id=> "0001033677"},
-    {:first=>"Christine", :last=>"Whitman", :cik_id=> "0001271384"},
-    {:first=>"Fareed", :last=>"Zakaria"},
-    {:middle=>"H", :first=>"Leslie", :last=>"Gelb", :cik_id=> "0001240500"},
-    {:middle=>"R", :first=>"Maurice", :last=>"Greenberg", :cik_id=> "0001236599"},
-    {:middle=>"G", :first=>"Peter", :last=>"Peterson", :cik_id=> "0001070843"}, 
-    {:first=>"David", :last=>"Rockefeller", :cik_id=> "0001204357"}
-]
-
 include SecQuery
 
 describe SecQuery::Entity do
-    puts "\n\nCOUNCIL ON FOREIGN RELATIONS' DIRECTORS\nAs perceived by the Security and Exchange Commission Edgar System:\n"
-    for t in cfr_directors
-        entity = SecQuery::Entity.find(t, true)
-        # entity = SecQuery::Entity.find(t, true, {:start => 0, :count => 20, :limit => 20}, {:start => 0, :count => 20, :limit => 20})
-        if entity != false
-          Entity.log(entity)
-          it t[:cik].to_s+" is company "+entity.name.to_s do    
-              if t[:cik] != nil
-                  entity.cik.should eql(t[:cik_id])
-              end
-             
-         end
+  puts "SecQuery Tests for all methods listed in the readme"
+
+  describe "Company Queries" do
+
+    let(:apple){{name: "APPLE INC",sic: "3571", symbol: "aapl", cik:"0000320193"}}
+
+    context "when quering by stock symbol" do
+      it "lazely" do  
+        entity = SecQuery::Entity.find(apple[:symbol])
+        entity.cik.should eql(apple[:cik])
+      end
+      it "explicitly" do
+        entity = SecQuery::Entity.find({:symbol=> apple[:symbol]})
+        entity.cik.should eql(apple[:cik])
+      end
+    end
+    
+    context "when querying by entity name" do
+      it "lazely" do  
+        entity = SecQuery::Entity.find(apple[:name])
+        entity.cik.should eql(apple[:cik])
+      end
+      it "explicitly" do
+        entity = SecQuery::Entity.find({:name=> apple[:name]})
+        entity.cik.should eql(apple[:cik])
+      end
+    end
+    
+    context "when querying by cik" do
+      it "lazely" do  
+        entity = SecQuery::Entity.find(apple[:cik])
+        entity.name.should match(apple[:name])
+      end
+      it "explicitly" do
+        entity = SecQuery::Entity.find({:cik=> apple[:cik]})
+        entity.name.should match(apple[:name])
+      end
+    end
+  end
+  
+  describe "People Queries" do
+
+    let(:jobs){{first: "Steve", middle: "P", last: "Jobs", :cik => "0001007844"}}
+  
+    context "when querying by name" do
+      it "first, middle and last name" do
+        entity = SecQuery::Entity.find({:first=> jobs[:first], :middle=> jobs[:middle], :last=> jobs[:last]})
+        entity.cik.should eql(jobs[:cik])
+      end
+    end
+  end
+  
+  describe "Relationships, Transactions and Filings" do
+    
+    ## Using Steve, because data should not change in the future. RIP.
+    
+    let(:jobs){{
+      first: "Steve", middle: "P", last: "Jobs", :cik => "0001007844",
+      relationships:[
+        {cik: "0000320193", name: "APPLE INC"},
+        {cik: "0001001039", name: "WALT DISNEY CO/"},
+        {cik: "0001002114", name: "PIXAR \\CA\\"}
+      ],
+      transactions:[
+        {filing_number: "0001181431-07-052839", reporting_owner: "APPLE INC", shares:120000.0},
+        {filing_number: "0001181431-07-052839", reporting_owner: "APPLE INC", shares: 40000.0},
+        {filing_number: "0001181431-07-052839", reporting_owner: "APPLE INC", shares: 40000.0},
+        {filing_number: "0001181431-07-052839", reporting_owner: "APPLE INC", shares: 40000.0},
+        {filing_number: "0001181431-06-028746", reporting_owner: "WALT DISNEY CO/", shares: 138000004.0},
+        {filing_number: "0001356184-06-000008", reporting_owner: "PIXAR \\CA\\", shares: 60000002.0},
+        {filing_number: "0001181431-06-019230", reporting_owner: "APPLE COMPUTER INC", shares: 4573553.0},
+        {filing_number: "0001181431-06-028747", reporting_owner: "WALT DISNEY CO/", shares: 0.0}
+      ],
+      filings:[
+        {cik: "0001007844", file_id: "0001181431-07-052839"},
+        {cik: "0001007844", file_id: "0001356184-06-000008"},
+        {cik: "0001007844", file_id: "0001193125-06-103741"},
+        {cik: "0001007844", file_id: "0001181431-06-028747"},
+        {cik: "0001007844", file_id: "0001181431-06-028746"},
+        {cik: "0001007844", file_id: "0001181431-06-019230"},
+        {cik: "0001007844", file_id: "0001193125-06-019727"},
+        {cik: "0001007844", file_id: "0001104659-03-004723"}
+      ]
+    }}
+    
+    let(:entity) {SecQuery::Entity.find({:first=> "Steve",  middle: "P", last: "Jobs", :cik => "0001007844"}, true)}
+    
+    context "when quering entities with option 'true'" do
+      it "should provide relationships" do
+        entity.relationships.each_with_index do |r, i|
+          r.cik.should eql(jobs[:relationships][i][:cik])
         end
-    end    
+      end
+      it "should provide transactions" do
+        entity.transactions.each_with_index do |t, i|
+          t.filing_number.should eql(jobs[:transactions][i][:filing_number])
+        end
+      end
+      it "should provide filings" do
+        entity.filings.each_with_index do |f, i|
+          f.file_id.should eql(jobs[:filings][i][:file_id])
+        end
+      end
+    end
+  end
 end
